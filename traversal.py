3606f15b66ae37b864a3ef57b0f0a7df40d91257
mmotaku <meng19941116@gmail.com>
2023-07-09 22:26:48 +0800

    meed traversal.py

--- traversal.py
+++ traversal.py
@@ -1,40 +1,18 @@
-# for string
-def traversal_str(s):
-    for i in range(len(s)):
-        print(s[i], end=' ')
+# for string, list, tuple
+def traversal(seq):
+    for i in range(len(seq)):
+        print(seq[i], end=' ')
     print()
 
-    for elem in s:
+    for elem in seq:
         print(elem, end=' ')
     print()
 
 
-# for list
-def traversal_list(lst):
-    for i in range(len(lst)):
-        print(lst[i], end=' ')
-    print()
-
-    for elem in lst:
-        print(elem, end=' ')
-    print()
-
-
-# for tuple
-def traversal_tuple(t):
-    for i in range(len(t)):
-        print(t[i], end=' ')
-    print()
-
-    for elem in t:
-        print(elem, end=' ')
-    print()
-
-
-# for map
-def traversal_map(m):
-    for key in m:
-        print(key, m[key])
+# for dict
+def traversal_dict(d):
+    for key in d:
+        print(key, d[key])
 
 
 # for set
@@ -44,16 +22,16 @@ def traversal_set(s):
 
 
 s = 'Hello, world'
-traversal_str(s)
+traversal(s)
 
 lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-traversal_list(lst)
+traversal(lst)
 
 t = (10, 11, 12, 13, 14, 15, 16, 17, 18, 19)
-traversal_tuple(t)
+traversal(t)
 
-m = {'a': 1, 'b': 2, 'c': 3}
-traversal_map(m)
+d = {'a': 1, 'b': 2, 'c': 3}
+traversal_dict(d)
 
 s = {1, 2, 3}
 traversal_set(s)
