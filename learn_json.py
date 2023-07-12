73c8cfe8d4d72ff917e95afddd2645768789481a
mmotaku <meng19941116@gmail.com>
2023-07-12 16:34:01 +0800

    mmjson

--- learn_json.py
+++ learn_json.py
@@ -7,5 +7,9 @@ f.close()
 print(sample)
 super_heroes = json.loads(sample)
 
-print(super_heroes['active'])
-print(super_heroes['members'][1]['powers'][2])
+print(super_heroes['secretBase'])
+print(super_heroes['members'][2]['powers'][3])
+
+sum = super_heroes['members'][0]['age'] + super_heroes['members'][1][
+    'age'] + super_heroes['members'][2]['age']
+print(sum)
